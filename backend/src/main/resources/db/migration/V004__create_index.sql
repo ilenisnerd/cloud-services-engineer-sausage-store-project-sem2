5b6dee56a222bf04edf5901d62b382adcd791fe5
ilenisnerd <ilenisnerd@yandex.ru>
2025-06-29 17:09:55 +0300

    fixing migration v4

--- backend/src/main/resources/db/migration/V004__create_index.sql
+++ backend/src/main/resources/db/migration/V004__create_index.sql
@@ -1,6 +1,6 @@
 -- migrations/V004__create_index.sql
 
 -- Индексы для оптимизации
-CREATE INDEX idx_order_product_product_id ON order_product(product_id);
-CREATE INDEX orders_status_date_idx ON orders(status, date_created); 
-CREATE INDEX order_product_order_id_idx ON order_product(order_id); 
+CREATE INDEX IF NOT EXISTS idx_order_product_product_id ON order_product(product_id);
+CREATE INDEX IF NOT EXISTS orders_status_date_idx ON orders(status, date_created); 
+CREATE INDEX IF NOT EXISTS order_product_order_id_idx ON order_product(order_id); 
